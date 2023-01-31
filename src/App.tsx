e0840d294277049874caf3277a2b07da5bc0f02d
Dan Garden <dangarden@gmail.com>
2023-01-31 21:01:12 +1000

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+interface DrawResult {
+  PrimaryNumbers: number[];
+  SecondaryNumbers: number[];
+}
+
+interface Data {
+  DrawResults: DrawResult[];
+}
+
+const App: React.FC = () => {
+  const [results, setResults] = useState<DrawResult[]>([]);
+  const [powerballNumbers, setPowerballNumbers] = useState<number[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://data.api.thelott.com/sales/vmax/web/data/lotto/latestresults', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          CompanyId: 'GoldenCasket',
+          MaxDrawCountPerProduct: 1,
+          OptionalProductFilter: ['Powerball']
+        })
+      });
+      const data = await response.json() as Data;
+
+      console.log(data);
+      setResults(data.DrawResults);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleAutoFill = () => {
+    const lastResult = results[0];
+    setPowerballNumbers(lastResult.PrimaryNumbers.concat(lastResult.SecondaryNumbers));
+  };
+
+  const handleClear = () => {
+    setPowerballNumbers([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Lott - Powerball Ticket</h1>
+      <div>
+        <h2>Selected Numbers:</h2>
+        {powerballNumbers.map(number => <div key={number}>{number}</div>)}
+      </div>
+      <div>
+        <button onClick={handleAutoFill}>AutoFill</button>
+        <button onClick={handleClear}>Clear</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
