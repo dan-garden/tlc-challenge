025caf83994d99768eb32d3c5b571e491a4447cf
Dan Garden <dangarden@gmail.com>
2023-02-01 22:25:12 +1000

    Adjusted some spacing and added favicons

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,9 @@ const App: React.FC = () => {
   return (
     <div className="p-5 lg:p-20">
 
-      <h1 className="text-2xl text-center mb-6">Number Selection</h1>
+      <h1 className="text-2xl text-center mb-6">Lotto Number Picker</h1>
 
-      <div className="border max-w-xl mx-auto w-full p-3 shadow-md rounded-md">
-        <NumberPicker
-          primaryCount={35}
-          primaryMax={7}
-          secondaryCount={20}
-          secondaryMax={1}
-        />
-      </div>
-
-      <br /><br />
-
-      <div className="border max-w-xl mx-auto w-full p-3 shadow-md rounded-md">
+      <div className="border max-w-xl mx-auto w-full p1 sm:p-3 shadow-md rounded-md bg-white">
         <NumberPicker
           primaryCount={35}
           primaryMax={7}
